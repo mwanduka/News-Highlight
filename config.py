c82cc62f58eebb4b015e88005d10c019bd860165
mwanduka <mwandukastephen20@gmail.com>
2018-11-09 12:08:03 +0300

    add api_key

--- config.py
+++ config.py
@@ -4,7 +4,12 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines/{}?api_Key={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 class ProdConfig(Config):
     '''
@@ -20,14 +25,14 @@ class TestConfig(Config):
     Testing configuration child class
 
     Args:
-        Config: The parent configuration class with General configuration settings 
+        Config: The parent configuration class with General configuration settings
     '''
     pass
 
 class DevConfig(Config):
     '''
     Development configuration child class
-    
+
     Args:
         Config: The parent configuration class with General configuration settings
     '''
