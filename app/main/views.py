02e94f966898507f9ffff2a3f2cd4d66d5d4d961
mwanduka <mwandukastephen20@gmail.com>
2018-11-10 12:36:16 +0300

    add news category

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
-from flask import render_template,request,redirect,url_for,abort
+from flask import render_template, request, redirect, url_for
 from . import main
+from ..request import get_source, search_source,get_article
 
 # Views
 @main.route('/')
@@ -8,22 +9,49 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-# Getting popular news
-    title = 'Home - Welcome to The Best news Review Website Online'
-    search_news = request.args.get('news_query')
+     #get sources
+    general = get_source('general')
+    technology = get_source('technology')
+    business = get_source('business')
+    entertainment = get_source('entertainment')
+    sports = get_source('sports')
 
-    if search_news:
-        return redirect(url_for('.search',news_name =search_news))
-    else:
-        return render_template('index.html', title = title, popular = popular_news, upcoming = upcoming_news, now_showing = now_showing_news )
+    title = 'Home - Welcome to the most informative news site online!!!'
 
-@main.route('/search/<news_name>')
-def search(news_name):
+    # search_source = request.args.get('search_query')
+
+    return render_template('index.html', title = title, general = general, technology = technology, business = business, entertainment = entertainment, sports = sports)
+
+
+
+@main.route('/search/<source_name>')
+def search(source_name):
     '''
-    View function to display the search results
+    View function to display search results
     '''
-    news_name_list = news_name.split(" ")
-    news_name_format = "+".join(news_name_list)
-    searched_news = search_news(news_name_format)
-    title = f'search results for(news_name)'
-    return render_template('search.html',news = searched_news)
+    source_name_list = source_name.split(" ")
+    source_name_format = "+".join(source_name_list)
+    searched_sources = search_source(source_name_format)
+    title = f'search results for {source_name}'
+
+    return render_template('search.html', sources = searched_sources)
+
+    #get articles
+    # article_general = get_article('general')
+    # print(article_general)
+
+    title = 'Home - Welcome to the most informative news site online!!!'
+    return render_template('index.html', title = title, general = source_general)
+
+
+
+@main.route('/source/<string:id>')
+def source(id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    source = get_article(id)
+    print(source)
+
+    return render_template('source.html',source = source)
