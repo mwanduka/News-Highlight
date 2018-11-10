1c2cb36c38a94281c20b0e8e49db375ca3cc6266
mwanduka <mwandukastephen20@gmail.com>
2018-11-10 12:35:12 +0300

    add api_key

--- config.py
+++ config.py
@@ -4,12 +4,9 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines/{}?api_Key={}'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-
-
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?category={}&apiKey={}'
+    NEWS_API_ARTICLE_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
 
 class ProdConfig(Config):
     '''
